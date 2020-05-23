4bd9393b8075ac5516b036ec77c5fbd52090f1da
Andrew Yates <andrewmacu@me.com>
2020-05-23 14:59:14 -0700

    init README and loading MNIST

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tensor Networks for MNIST Classification
+
+A short demonstration of Google's TensorNetwork library (https://arxiv.org/pdf/1905.01330) for the classification of MNIST.
+The goal is to employ a few different architectures for this:
+
+1. Matrix product states (https://arxiv.org/pdf/1906.06329).
+2. Tree tensor networks (https://arxiv.org/abs/1901.02217 and https://arxiv.org/abs/1801.00315).
+3. Implementing architectures (1) and (2) in a quantum circuit (https://arxiv.org/pdf/1803.11537).
+
+Motivating Question: In machine learning, tensors are used, where they are usually treated as multidimensional arrays. 
+In physics, tensors are also used, where they are treated as multilinear maps invariant under coordinate transformations.
+Can we use the extensive tensor network literature developed in physics to improve machine learning? (For more information
+by experts, please see the references I've linked above.)
+
+I'll demonstrate a few strategies for doing MNIST classification tasks using tensor networks. Namely, the matrix product state and
+tree tensor network. The MPS architecture doesn't preserve the dimensionality of the image, while the TTN
+does to some extent.
---
+++ main.py
@@ -0,0 +1,45 @@
+import numpy as np
+import tensornetwork as tn
+import os
+import mnist
+import matplotlib
+import matplotlib.pyplot as plt
+import pickle
+
+mnist_data_directory = os.path.join(os.path.dirname(__file__), "data")
+
+
+def load_MNIST_dataset():
+    """
+    Loads the MNIST dataset from data/ which has been gitignored.
+    Credit: This function alone has been adapted from Cornell's CS 4787
+    Large Scale Machine Learning course.
+    :return:
+    """
+    PICKLE_FILE = os.path.join(mnist_data_directory, "MNIST.pickle")
+    try:
+        dataset = pickle.load(open(PICKLE_FILE, 'rb'))
+    except:
+        # load the MNIST dataset
+        mnist_data = mnist.MNIST(mnist_data_directory, return_type="numpy", gz=True)
+        Xs_tr, Lbls_tr = mnist_data.load_training()
+        Xs_tr = Xs_tr.transpose() / 255.0
+        Ys_tr = np.zeros((10, 60000))
+        for i in range(60000):
+            Ys_tr[Lbls_tr[i], i] = 1.0  # one-hot encode each label
+        Xs_tr = np.ascontiguousarray(Xs_tr)
+        Ys_tr = np.ascontiguousarray(Ys_tr)
+        Xs_te, Lbls_te = mnist_data.load_testing()
+        Xs_te = Xs_te.transpose() / 255.0
+        Ys_te = np.zeros((10, 10000))
+        for i in range(10000):
+            Ys_te[Lbls_te[i], i] = 1.0  # one-hot encode each label
+        Xs_te = np.ascontiguousarray(Xs_te)
+        Ys_te = np.ascontiguousarray(Ys_te)
+        dataset = (Xs_tr, Ys_tr, Xs_te, Ys_te)
+        pickle.dump(dataset, open(PICKLE_FILE, 'wb'))
+    return dataset
+
+
+if __name__ == "__main__":
+    (Xs_tr, Ys_tr, Xs_te, Ys_te) = load_MNIST_dataset()
